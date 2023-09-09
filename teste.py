c67ec31031f62ccf42d1acfe6795d75aafb66e6b
Guilhermehzf <guilhermehzf@gmail.com>
2023-09-08 22:08:35 -0300

    klhjgse

--- teste.py
+++ teste.py
@@ -2,3 +2,17 @@ print("hello world")
 "fkfjsdçaklfja"
 "fdkgsakjçglkaj"
 'çlkgjfdeç~l'
+kjghdsalkjghalkjghadslkjg
+[gçdaslkjljhgalwsekjhgas
+ bnjgadskmghbaçkjh
+ ç~ç~kgçasdkjhgasçlkghjas~çlkg
+ çl
+ lg
+ sghhsa~klghnas
+ çl~kas
+ ~çhgkasd
+ çlhjasd
+ çljads
+ çlhja
+ phkl
+ çl]
